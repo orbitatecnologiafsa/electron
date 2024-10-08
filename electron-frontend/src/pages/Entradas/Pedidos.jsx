680a0202e10b87097af29d1c6c92f980886f410f
orbitatecnologiafsa <orbitatecnologiafsa@gmail.com>
2024-10-08 17:42:24 -0300

    Criação de paginas da aba cadastro/entrada/saida. Algumas ediçoes nas paginas de cada aba - Alisson

--- electron-frontend/src/pages/Entradas/Pedidos.jsx
+++ electron-frontend/src/pages/Entradas/Pedidos.jsx
@@ -47,12 +47,12 @@ function PedidoDeCompra() {
                     <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md w-1/5" >
                       <span className="flex select-none items-center pl-3 text-gray-500 sm:text-sm">
                       </span>
-                      <input type="text" id="input2" className="block w-full ml-3 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"  />
+                      <input type="text" id="input3" className="block w-full ml-3 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"  />
                     </div>
                     <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md w-1/5" >
                       <span className="flex select-none items-center pl-3 text-gray-500 sm:text-sm">
                       </span>
-                      <input type="text" id="input2" className="block w-full ml-3 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"  />
+                      <input type="text" id="input4" className="block w-full ml-3 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"  />
                     </div>
                   </div>
                   </div>
