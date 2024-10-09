981ed698d1d0e3ffd15690235a8fbb31427cb45f
orbitatecnologiafsa <orbitatecnologiafsa@gmail.com>
2024-10-09 17:46:36 -0300

    início configuração de tokens jwt

--- electron-backend/src/main/java/com/electron/domain/Usuario.java
+++ electron-backend/src/main/java/com/electron/domain/Usuario.java
@@ -2,16 +2,24 @@ package com.electron.domain;
 
 import com.electron.domain.enums.Cargo;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Table(name = "usuarios")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class Usuario {
+public class Usuario implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -20,9 +28,50 @@ public class Usuario {
     @Column(unique = true)
     private String email;
 
+    @NotNull
+    @Size(min = 6, max = 14)
+    private String senha;
+
+    @NotNull
     private String nome;
 
     @Enumerated(EnumType.STRING)
     private Cargo cargo = Cargo.USUARIO;
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        if (this.cargo == Cargo.ADMIN)
+            return List.of(new SimpleGrantedAuthority("ADMIN"), new SimpleGrantedAuthority("USUARIO"));
+        else return List.of(new SimpleGrantedAuthority("USUARIO"));
+    }
+
+    @Override
+    public String getPassword() {
+        return "";
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
