cb82636b392f8a11e042f2c7845db52a274f89a9
Orbita Tecnologia <johanvictor17@gmail.com>
2024-10-31 17:13:49 -0300

    Tratamento de exceções

---
+++ electron-backend/src/main/java/com/electron/controllers/exceptions/ControllerExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.electron.controllers.exceptions;
+
+import com.electron.services.exceptions.NotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.Instant;
+
+@ControllerAdvice
+public class ControllerExceptionHandler {
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<StandardError> resourceNotFound(NotFoundException e, HttpServletRequest request) {
+        String error = "Resource not found";
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+}
